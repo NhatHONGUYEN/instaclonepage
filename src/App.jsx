1dcd601239ed09f25b3ed170b1ba5783518f7273
NhatHONGUYEN <hnnhat67@gmail.com>
2024-07-17 12:08:18 +0200

    ✨ F01 : Add login and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/signUp" element={<SignUp />} />
     </Routes>
   );
 }
