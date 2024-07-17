410c9e1a66164f0fe6da4e602b0d94c6eda370d2
NhatHONGUYEN <hnnhat67@gmail.com>
2024-07-17 11:57:07 +0200

    💄 ✨ F01 : finish the layout of our homepage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,28 @@
 export default function NavBar() {
   return (
-    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-7xl flex justify-center items-center sm:px-6 lg:px-8">
       <div className="avatar">
-        <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2 ml-4">
+        <div className="ring-primary ring-offset-base-100 w-14 rounded-full ring ring-offset-2 ml-6">
           <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
         </div>
       </div>
       <div className="avatar">
-        <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2 ml-4">
+        <div className="ring-primary ring-offset-base-100 w-14 rounded-full ring ring-offset-2 ml-6">
           <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
         </div>
       </div>
       <div className="avatar">
-        <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2 ml-4">
+        <div className="ring-primary ring-offset-base-100 w-14 rounded-full ring ring-offset-2 ml-6">
           <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
         </div>
       </div>
       <div className="avatar">
-        <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2 ml-4">
+        <div className="ring-primary ring-offset-base-100 w-14 rounded-full ring ring-offset-2 ml-6">
           <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
         </div>
       </div>
       <div className="avatar">
-        <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2 ml-4">
-          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-        </div>
-      </div>
-      <div className="avatar">
-        <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2 ml-4">
+        <div className="ring-primary ring-offset-base-100 w-14 rounded-full ring ring-offset-2 ml-6">
           <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
         </div>
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,59 +2,70 @@ import { Dot, Heart, MessageCircle, Send, Smile } from "lucide-react";
 
 export default function Post() {
   return (
-    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <div className="avatar">
-          <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+    <div className="mx-auto max-w-7xl mt-20 sm:px-6 flex justify-center items-center lg:px-8">
+      <div className="card bg-base-100 max-w-xl shadow-xl ">
+        <div className="flex w-full flex-col gap-4">
+          <div className="flex  items-center gap-4">
+            <div className=" h-10 w-10 shrink-0 ring-primary ring-offset-base-100 rounded-full ring ring-offset-2">
+              <img
+                className="rounded-full"
+                src="../../public/nhatprofilinsta.jpg"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <div className="flex text-sm font-semibold h-4 w-20">
+                Nhatflix <Dot /> 5j
+              </div>
+              <div className="h-4 w-28">Audio d'origine</div>
+            </div>
           </div>
-          <p>
-            nhatflix <Dot /> 3j
-          </p>
-          <p>Audio d'origine</p>
+          <div className=" h-[600px]  w-full">
+            <video
+              className="h-full object-cover w-full"
+              autoPlay
+              muted
+              loop
+              controls={false}
+              src="/Womanemptyfav.mp4"
+            ></video>
+          </div>
         </div>
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
-        </figure>
         <div className="card-body">
-          <Heart /> <MessageCircle /> <Send />
-          <div className="avatar-group -space-x-6 rtl:space-x-reverse">
-            <div className="avatar">
-              <div className="w-12">
-                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          <div className="flex gap-3 items-center">
+            <Heart /> <MessageCircle /> <Send />
+          </div>
+          <div className="flex ">
+            <div className="avatar-group -space-x-6 rtl:space-x-reverse">
+              <div className="avatar"></div>
+              <div className="avatar">
+                <div className="w-5 h-5 ml-2">
+                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
               </div>
-            </div>
-            <div className="avatar">
-              <div className="w-12">
-                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <div className="avatar">
+                <div className="w-5 h-5 ml-2">
+                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
               </div>
-            </div>
-            <div className="avatar">
-              <div className="w-12">
-                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <div className="avatar">
+                <div className="w-5 h-5  ml-2">
+                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
               </div>
+              <p className="pl-8 font-semibold">308 J'aime</p>
             </div>
-            <div className="avatar">
-              <div className="w-12">
-                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
-            </div>
-            <p>308 J'aime</p>
           </div>
           <h2 className="card-title">
             joel_brown01 A small excerpt of my judge demo of yesterday at
           </h2>
           <p>Afficher les 43 commentaires</p>
-          <p>
+          <p className="flex justify-between">
             leetheruggeds The best life <Heart size={16} strokeWidth={0.5} />
           </p>
-          <p>
+          <p className="flex justify-between">
             littleshao My man ❤️ <Heart size={16} strokeWidth={0.5} />
           </p>
-          <div className="card-actions justify-end">
+          <div className="card-actions flex justify-between items-center">
             <input
               type="text"
               placeholder="Ajouter un commentaire..."
